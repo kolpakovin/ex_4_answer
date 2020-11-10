c57e7243596159318e6ce39f1cff5ebf13a285f4
Evgeny Kolpakov <kolpakovin@gmail.com>
2020-11-10 12:42:34 +0200

    indentation and missing;

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 class AnimalFarm {
-    constructor() {
+    constructor(farmIdentifier) {
+        this.farm = farmIdentifier; // In case we want to make more farms in the future
         this.removedAnimals = [];  // Here I'll store all removed animal
     }
     
@@ -20,51 +21,61 @@ class AnimalFarm {
                     console.error("We don't support the creation of this animal");
                     return;
             };
-            aList.appendChild(createdAnimal.element)
+            this.farm.appendChild(createdAnimal.element);
         });
     };
+
     removeAnimalOnClick(event) {
-        const animals = Array.from(aList.children)
+        const parentNodeId = event.currentTarget.parentNode.id;
+        const animals = Array.from(animalFarm.farm.children);
         const index = animals.indexOf(event.currentTarget);
-        if(index === -1) {
-            console.log("Can't remove the right animal")
-        }
         // I want to know index of the removed element because the animals order is important 
         // for me and I'll use it when I'll return all removed animals
-        animalFarm.removedAnimals.push({index: index, element: event.currentTarget});
-        aList.removeChild(event.currentTarget);
-    }
+        if(index === -1) {
+            console.error("Can't remove the right animal")
+        };
+        switch(parentNodeId) {
+            case "Animals_div":
+                animalFarm.removedAnimals.push({index: index, element: event.currentTarget});
+                animalFarm.farm.removeChild(event.currentTarget);
+                break;
+            default:
+                console.error("No such farm exists");
+                return;
+        };
+    };
 
     returnDeletedAnimals() {
         for(let i = this.removedAnimals.length - 1; i >= 0; i--){ // Here I use the index to return the animal to its place
-            aList.insertBefore(this.removedAnimals[i].element, aList.children[this.removedAnimals[i].index])
+            this.farm.insertBefore(this.removedAnimals[i].element, aList.children[this.removedAnimals[i].index]);
         }
         this.removedAnimals = [];
-    }
+    };
     
     leaveOnlyPenguin() {
         const animals = Array.from(aList.children)
         animals.forEach(animal => {
-            if(animal.attributes[0].value === 'rabbit' || animal.attributes[0].value === 'elephant') {
+            if (animal.attributes[0].value === 'rabbit' || animal.attributes[0].value === 'elephant') {
                 this.removeAnimal(animal);
-            }
-        })
-    }
+            };
+        });
+    };
     
     removeAnimal(animal) {
-        const animals = Array.from(aList.children)
+        const animals = Array.from(this.farm.children)
         const index = animals.indexOf(animal)
         this.removedAnimals.push({index: index, element: animal})
-        aList.removeChild(animal);
-    }
+        this.farm.removeChild(animal);
+    };
 };
 
-const animalFarm = new AnimalFarm();
+let aList = document.getElementById("Animals_div"); 
+const animalFarm = new AnimalFarm(aList);
 
 class Animal {
     constructor(specialProperty) {
         this.specialProperty = specialProperty;
-    }
+    };
 
     createElement() {
         this.element = document.createElement("div");
@@ -77,8 +88,8 @@ class Animal {
         this.element.appendChild(atext);
         this.element.addEventListener('click', animalFarm.removeAnimalOnClick);
         return this.element;
-    }
-}
+    };
+};
 
 class Penguin extends Animal {
     constructor(specialProperty) {
@@ -87,8 +98,8 @@ class Penguin extends Animal {
         this.note = "The Penguin Swimming Speed is:" + this.specialProperty + "km/h";
         this.image = "./images/img03.jpg";
         this.createElement();
-    }
-}
+    };
+};
 
 class Rabbit extends Animal {
     constructor(specialProperty) {
@@ -97,8 +108,8 @@ class Rabbit extends Animal {
         this.note = "The Rabbit Speed is:" + this.specialProperty + "km/h";
         this.image = "./images/img02.jpg";
         this.createElement();
-    }
-}
+    };
+};
 
 class Elephant extends Animal{
     constructor(specialProperty) {
@@ -107,10 +118,9 @@ class Elephant extends Animal{
         this.note = "The Elephant Weight is:" + this.specialProperty + "kg";
         this.image = "./images/img01.jpg";
         this.createElement();
-    }
-}
+    };
+};
 
-let aList = document.getElementById("Animals_div");
 
 let aAnimal1 = new Elephant(780);
 let aAnimal2 = new Elephant(650);
